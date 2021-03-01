b43f5a019206a580c6c1c08488d8d04e7d39834d
Fernando <fernandoselvino@gmail.com>
2021-03-01 20:00:45 -0300

    Registrando o fromulario

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Register = () => {
+
+    const [email, setEmail] = useState("")
+
+    const handleSubmit = () => {}
+
+    const registerForm = () => ( 
+    <form onSubmit={handleSubmit}>
+        <input 
+        type="email"
+        className="form-control"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        autoFocus
+        />
+        <button type ="button"className="btn btn-raised">
+            Regiter / {email}
+        </button>
+    </form>)
+
+
     return (
-        <div>
-            <p>Register</p>
-        </div>
+       <div className="container p-5">         
+             <div className="row"> 
+                 <div className="col-md-6 offset-md-3">
+                     <h4>Register</h4>
+                    {registerForm()}
+                </div>
+            </div> 
+      </div>
     )
 }
 
