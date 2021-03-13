a6baa507bef71a165f6a935f81538df2551f77f4
fernandoselvino@gmail.com <fernandoselvino@gmail.com>
2021-03-13 15:37:50 -0300

    Aula 16 ENV and Toast Notifications

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import Register from './pages/auth/Register'
 import Home from './pages/Home'
 import Header from './components/nav/Header'
 import 'antd/dist/antd.css'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 
 const App = () => {
   return (
     <>
     <Header />
+    <ToastContainer/> 
     <Switch>
       <Route path="/" exact component={Home}/>
       <Route path="/login" component={Login}/>
