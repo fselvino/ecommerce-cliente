410f60a68ad2a59429d55da44c7bdc6f6d44d012
fernandoselvino@gmail.com <fernandoselvino@gmail.com>
2021-03-13 13:29:19 -0300

    Aula 15 Email link to Sigin

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,10 +1,28 @@
 import React,{useState} from 'react'
+import {auth} from '../../firebase'
+import {toast, ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const Register = () => {
 
     const [email, setEmail] = useState("")
 
-    const handleSubmit = () => {}
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const config = {
+            url: 'http://localhost:3000/register/complete',
+            handleCodeInApp: true
+        }
+        await auth.sendSignInLinkToEmail(email, config)
+        toast.success(`Email is sent to ${email}. Click the link to complete your registration`)
+
+        //Save user email in local storage
+        window.localStorage.setItem('emailForRegistration', email)
+
+        //clear state
+        setEmail("")
+    }
 
     const registerForm = () => ( 
     <form onSubmit={handleSubmit}>
@@ -15,8 +33,8 @@ const Register = () => {
         onChange={(e) => setEmail(e.target.value)}
         autoFocus
         />
-        <button type ="button"className="btn btn-raised">
-            Regiter / {email}
+        <button type ="submit"className="btn btn-raised">
+            Regiter 
         </button>
     </form>)
 
@@ -26,6 +44,7 @@ const Register = () => {
              <div className="row"> 
                  <div className="col-md-6 offset-md-3">
                      <h4>Register</h4>
+                     <ToastContainer/>                    
                     {registerForm()}
                 </div>
             </div> 
